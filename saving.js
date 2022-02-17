b9e31bb7dcac0aeb7a9d2370a99c7f06341da618
iseau395 <70857830+iseau395@users.noreply.github.com>
2022-02-16 16:15:05 -0800

    made getting the default save less stupid
    closes #7

--- saving.js
+++ saving.js
@@ -3,52 +3,7 @@ import { points, gameobjects, slot } from "./map.js";
 
 const FIELD_SIDE = 713.74;
 
-// This isn't used
-const starting_gameobjects = [
-    new Mogo(
-        FIELD_SIDE / 2,
-        FIELD_SIDE / 4,
-        180,
-        0
-    ),
-    new Mogo(
-        FIELD_SIDE / 2,
-        (FIELD_SIDE / 4) * 2,
-        0,
-        0
-    ),
-    new Mogo(
-        FIELD_SIDE / 2,
-        (FIELD_SIDE / 4) * 3,
-        0,
-        0
-    ),
-    new Mogo(
-        (FIELD_SIDE / 12) * 3,
-        (FIELD_SIDE / 12) * 11,
-        270,
-        1
-    ),
-    new Mogo(
-        (FIELD_SIDE / 12) * 1,
-        (FIELD_SIDE / 6) * 2,
-        0,
-        1
-    ),
-    new Mogo(
-        (FIELD_SIDE / 12) * 9,
-        (FIELD_SIDE / 12) * 1,
-        90,
-        2
-    ),
-    new Mogo(
-        (FIELD_SIDE / 12) * 11,
-        (FIELD_SIDE / 3) * 2,
-        180,
-        2
-    ),
-    new Ring(FIELD_SIDE / 2, 29.708333333333332), new Ring(FIELD_SIDE / 2, 59.416666666666664), new Ring(FIELD_SIDE / 2, 89.125), new Ring(FIELD_SIDE / 2, FIELD_SIDE / 6), new Ring(386.2083333333333, FIELD_SIDE / 6), new Ring(415.91666666666663, FIELD_SIDE / 6), new Ring(445.625, FIELD_SIDE / 6), new Ring(475.3333333333333, FIELD_SIDE / 6), new Ring(FIELD_SIDE / 2, 237.66666666666666), new Ring(FIELD_SIDE / 2, 267.375), new Ring(FIELD_SIDE / 2, 297.0833333333333), new Ring(FIELD_SIDE / 2, 475.3333333333333), new Ring(FIELD_SIDE / 2, 445.625), new Ring(FIELD_SIDE / 2, 445.625), new Ring(FIELD_SIDE / 2, 415.91666666666663), new Ring(FIELD_SIDE / 2, 594.1666666666666), new Ring(FIELD_SIDE / 2, 623.875), new Ring(FIELD_SIDE / 2, 653.5833333333333), new Ring(FIELD_SIDE / 2, 683.2916666666666), new Ring(326.79166666666663, 594.1666666666666), new Ring(297.0833333333333, 594.1666666666666), new Ring(267.375, 594.1666666666666), new Ring(237.66666666666666, 594.1666666666666), new Ring(475.3333333333333, FIELD_SIDE / 2), new Ring(490.1875, FIELD_SIDE / 2), new Ring(475.3333333333333, 341.6458333333333), new Ring(460.47916666666663, FIELD_SIDE / 2), new Ring(475.3333333333333, 371.35416666666663), new Ring(475.3333333333333, 475.3333333333333), new Ring(490.1875, 475.3333333333333), new Ring(475.3333333333333, 460.47916666666663), new Ring(460.47916666666663, 475.3333333333333), new Ring(475.3333333333333, 490.1875), new Ring(237.66666666666666, FIELD_SIDE / 2), new Ring(252.52083333333331, FIELD_SIDE / 2), new Ring(222.8125, FIELD_SIDE / 2), new Ring(237.66666666666666, 341.6458333333333), new Ring(237.66666666666666, 371.35416666666663), new Ring(237.66666666666666, 237.66666666666666), new Ring(237.66666666666666, 222.8125), new Ring(252.52083333333331, 237.66666666666666), new Ring(237.66666666666666, 252.52083333333331), new Ring(222.8125, 237.66666666666666)
-];
+const default_save = "|mogo-356.50-178.25-180-0/mogo-356.50-356.50-0-0/mogo-356.50-534.75-0-0/mogo-178.25-653.58-270-1/mogo-59.42-237.67-0-1/mogo-534.75-59.42-90-2/mogo-653.58-475.33-180-2/ring-356.50-29.71/ring-356.50-59.42/ring-356.50-89.13/ring-356.50-118.83/ring-386.21-118.83/ring-415.92-118.83/ring-445.63-118.83/ring-475.33-118.83/ring-356.50-237.67/ring-356.50-267.38/ring-356.50-297.08/ring-356.50-475.33/ring-356.50-445.63/ring-356.50-445.63/ring-356.50-415.92/ring-356.50-594.17/ring-356.50-623.88/ring-356.50-653.58/ring-356.50-683.29/ring-326.79-594.17/ring-297.08-594.17/ring-267.38-594.17/ring-237.67-594.17/ring-475.33-356.50/ring-490.19-356.50/ring-475.33-341.65/ring-460.48-356.50/ring-475.33-371.35/ring-475.33-475.33/ring-490.19-475.33/ring-475.33-460.48/ring-460.48-475.33/ring-475.33-490.19/ring-237.67-356.50/ring-252.52-356.50/ring-222.81-356.50/ring-237.67-341.65/ring-237.67-371.35/ring-237.67-237.67/ring-237.67-222.81/ring-252.52-237.67/ring-237.67-252.52/ring-222.81-237.67/";
 
 document.getElementById("clear-button").addEventListener("click", () => {
     localStorage.removeItem(slot);
@@ -111,7 +66,7 @@ export async function load(slot) {
         points.length = 0;
         gameobjects.length = 0;
 
-        raw = await (await (await fetch("./default_save")).text()).split("|");
+        raw = default_save.split("|");
     };
 
     let raw_points = raw[0].split("/");
