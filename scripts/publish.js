673389563d4ad0364f1756cba3c628e304439495
iseau395 <70857830+iseau395@users.noreply.github.com>
2022-03-01 17:16:08 -0800

    made publish work better

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,16 +1,4 @@
 const ghpages = require('gh-pages');
-const { exec } = require("child_process");
-
-exec("npm run build", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`${stderr}`);
-        return;
-    }
-});
 
 ghpages.publish('public', 
     {
