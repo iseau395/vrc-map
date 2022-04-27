897e71e7b4068f543f8cba74b707b07f1aefa32d
iseau395 <70857830+iseau395@users.noreply.github.com>
2022-04-26 20:30:24 -0700

    Add issue templates and publish script

---
+++ scripts/publish.js
@@ -0,0 +1,12 @@
+const ghpages = require('gh-pages');
+
+ghpages.publish('public', 
+    {
+        branch: 'gh-pages',
+        repo: 'https://github.com/iseau395/vrc-map.git',
+        message: 'Automatically update github pages branch'
+    },
+    (err) => {
+        if (err) console.error(err);
+    }
+);
