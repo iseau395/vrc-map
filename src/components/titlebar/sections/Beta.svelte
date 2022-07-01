c94f8fbcf7a4df3cb8d723c03d24c3320d96e868
iseau395 <70857830+iseau395@users.noreply.github.com>
2022-06-30 21:55:36 -0700

    Update beta button on titlebar to have an updated link and look a little better

--- src/components/titlebar/sections/Beta.svelte
+++ src/components/titlebar/sections/Beta.svelte
@@ -3,5 +3,16 @@
 </script>
 
 <p>Disclamer: The beta is not finished yet</p>
-<Link label="Link" link="https://iseau395.github.io/vrc-map/beta/tipping-point/"/>
+<Link label="Link" link="https://iseau395.github.io/vrc-map/beta/spin-up/"/>
 <Link label="Github Repository" link="https://github.com/iseau395/vrc-map/tree/beta/"/>
+
+<style>
+    p {
+        color: black;
+        margin: 0px;
+
+        border-style: solid;
+        border-width: 0px;
+        border-bottom-width: 1px;
+    }
+</style>
